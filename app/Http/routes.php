27fbcff2627d9555e6db18192ef07edd917b7022
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-07-01 13:48:44 -0500

    Advances CRUD Medication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,19 +8,21 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
-// patient routes
+// Patient routes
 Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/pacientes', 'PatientController@index');
-    Route::get('/pacientes/registrar', 'PatientController@create');
     Route::post('/pacientes/registrar', 'PatientController@store');
     Route::post('/pacientes/modificar', 'PatientController@edit');
     Route::post('/pacientes/eliminar', 'PatientController@delete');
 
-
 // Symptom routes
 
 // Medication routes
+    Route::get('/medicamentos', 'MedicationController@index');
+    Route::post('/medicamentos/registrar', 'MedicationController@store');
+    Route::post('/medicamentos/modificar', 'MedicationController@edit');
+    Route::post('/medicamentos/eliminar', 'MedicationController@delete');
 
 // Disease routes
 
