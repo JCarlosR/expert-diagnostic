2825f6a530292dcf1865a22f9dbd28f29ea3cee4
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-07-13 07:21:10 -0500

    Seeders medications

--- database/seeds/MedicationsTableSeeder.php
+++ database/seeds/MedicationsTableSeeder.php
@@ -75,5 +75,124 @@ class MedicationsTableSeeder extends Seeder
             'image' => '9.jpg'
         ]);
 
+        Medication::create([
+            'active_component' => 'Paracetamol',
+            'trade_name' => 'Panadol',
+            'description' => 'Panadol Extra Fuerte proporciona alivio efectivo de dolores y molestias, tales como dolores de cabeza y dolor post tratamientos dentales / extracción dental.',
+            'image' => '10.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Metamizol Sódico Monohidrato',
+            'trade_name' => 'Novalgina',
+            'description' => 'Novalgina es un potente analgésico antipirético y antiinflamatorio con metamizol como principio activo. Este medicamento inhibe la producción de prostaglandinas, unas sustancias endógenas responsables del dolor, las reacciones inflamatorias y el aumento de la temperatura corporal.',
+            'image' => '11.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Metamizol sódico',
+            'trade_name' => 'Repriman',
+            'description' => 'Fiebre alta de diversa etiología, antecedente de convulsiones febriles, dolor de origen tumoral, dolor intenso que no cede a otras medidas.',
+            'image' => '12.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Solución Salina Normal',
+            'trade_name' => 'Suero',
+            'description' => 'El término suero es un término que se utiliza para designar a algunos tipos de líquidos con características y usos específicos. En este sentido, el suero puede ser una solución salina que se utiliza en la medicina como complemento nutriente para personas o animales convalecientes.',
+            'image' => '13.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Glucosa, Cloruro de potasio, Cloruro de sodio, Citrato trisódico dihidratado.',
+            'trade_name' => 'Suero Oral',
+            'description' => 'Los Electrolitos son sales y minerales que se convierten en iones en agua y adquiere la capacidad de conducir electricidad. Estos iones producen un impulso eléctrico que viaja por nuestro cuerpo. Los electrolitos entran y salen de las células.',
+            'image' => '14.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Hidróxido de Aluminio.',
+            'trade_name' => 'Hidróxido de aluminio',
+            'description' => 'El hidróxido de aluminio y el hidróxido de magnesio son los antiácidos usados juntos para aliviar la pirosis (acidez o calor estomacal), la indigestión ácida y los malestares estomacales. Pueden usarse para tratar estos síntomas en los pacientes con úlcera péptica, gastritis, esofagitis, hernia hiatal o demasiado ácido en el estómago (hiperacidez gástrica).',
+            'image' => '15.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Hidróxido de magnesio, hidróxido de aluminio, simeticona.',
+            'trade_name' => 'Mylanta',
+            'description' => 'Es un antiácido. Los antiácidos actúan neutralizando el ácido suelto en el estómago, el ácido ya producido, brindando alivio rápido en sólo minutos.',
+            'image' => '16.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Vitamina B1, Vitamina B2, Vitamina B6, Niacina, Vitamina B12, Ácido fólico, Ácido pantoténico.',
+            'trade_name' => 'Complejo B',
+            'description' => 'Es una fórmula nutritivamente equilibrada de siete vitaminas B esenciales, seis de ellas principalmente de origen natural.',
+            'image' => '17.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Vitamina C.',
+            'trade_name' => 'Vitamina C',
+            'description' => 'Conocida como ácido ascórbico, es un nutriente hidrosoluble. Es la vitamina menos estable y es muy sensible a reaccionar con el oxígeno. Esto es importante ya que su potencia puede perderse cuando se oxida por exposición a la luz, el calor y el aire.',
+            'image' => '18.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Silimarina, Tiamina HCl, Riboflavina, Nicotinamida, Piridoxina HCl, Pantotenato de calcio',
+            'trade_name' => 'Higanatur B',
+            'description' => 'Está indicado en casos de intoxicación por Amanita phalloides, coadyuvante en el tratamiento de la intoxicación por Tetracloruro de Carbono, y en casos de Hepatitis Alcohólica.',
+            'image' => '19.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Glicerina',
+            'trade_name' => 'Supositrios',
+            'description' => 'Está indicado para la constipación ocasional.',
+            'image' => '20.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Simeticona',
+            'trade_name' => 'Gaseoved',
+            'description' => 'Es un efectivo antiflatulento, a base de Simeticona, que es una silicona cuya acción es alterar la tensión superficial de las burbujas de gases que se forman en el tracto gastrointestinal, ya sea en el proceso digestivo mismo o por intolerancia a la lactosa.',
+            'image' => '21.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Enzimas pancreáticas',
+            'trade_name' => 'Frutenzima',
+            'description' => 'Gastroparesia diabética aguda y recidivante. Profilaxis de náuseas y vómitos inducidos por la quimioterapia. Tratamiento a corto plazo de pirosis y del vaciado gástrico retardado, secundarios a la esofagitis por reflujo. Coadyuvante de la radiografía gastrointestinal.',
+            'image' => '22.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Pancreatina, Simeticona',
+            'trade_name' => 'Pankreoflat',
+            'description' => 'Pankreoflat es útil para el alivio sintomático de las alteraciones digestivas en las que se produce aerofagia (deglución de aire) o flatulencia (gases), como pesadez de estómago y digestiones lentas relacionadas con insuciencia de enzimas pancreáticas.',
+            'image' => '23.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Picosulfato sódico',
+            'trade_name' => 'Dibrolax',
+            'description' => 'Laxante estimulante de acción local. Aumenta el peristaltismo en intestino grueso y el contenido de agua y electrolitos en luz intestinal del colon.',
+            'image' => '0.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Bisacodilo',
+            'trade_name' => 'Dulcolax',
+            'description' => 'Son ideales para las personas que buscan alivio del estreñimiento durante la noche. En todo el mundo se consideran como suaves, confiables y fáciles de tomar.',
+            'image' => '24.jpg'
+        ]);
+
+        Medication::create([
+            'active_component' => 'Simeticona',
+            'trade_name' => 'Digestal',
+            'description' => 'Está recomendada en el tratamiento sintomático del síndrome de mala absorción causado por insuficiencia pancreática de origen orgánico, en la fibrosis quística del páncreas, en pancreatitis crónica y en otros estados en que la insuficiencia pancreática desequilibre la digestión de grasas.',
+            'image' => '25.jpg'
+        ]);
+
     }
 }
