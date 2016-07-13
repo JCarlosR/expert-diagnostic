e4beb14d0915050d7448dba81ef551d43df80c02
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-07-13 11:08:56 -0500

    Seeders Disease-Medications

--- database/seeds/DiseaseMedicationSeeder.php
+++ database/seeds/DiseaseMedicationSeeder.php
@@ -23,5 +23,54 @@ class DiseaseMedicationsSeeder extends Seeder
         DiseaseMedication::create([ 'disease_id' => 3, 'medication_id' => 6 ]);
         DiseaseMedication::create([ 'disease_id' => 3, 'medication_id' => 7 ]);
         DiseaseMedication::create([ 'disease_id' => 3, 'medication_id' => 9 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 4, 'medication_id' => 3 ]);
+        DiseaseMedication::create([ 'disease_id' => 4, 'medication_id' => 4 ]);
+        DiseaseMedication::create([ 'disease_id' => 4, 'medication_id' => 13 ]);
+        DiseaseMedication::create([ 'disease_id' => 4, 'medication_id' => 14 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 5, 'medication_id' => 16 ]);
+        DiseaseMedication::create([ 'disease_id' => 5, 'medication_id' => 15 ]);
+        DiseaseMedication::create([ 'disease_id' => 5, 'medication_id' => 14 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 6 ]);
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 5 ]);
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 1 ]);
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 2 ]);
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 18 ]);
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 17 ]);
+        DiseaseMedication::create([ 'disease_id' => 6, 'medication_id' => 7 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 7, 'medication_id' => 7 ]);
+        DiseaseMedication::create([ 'disease_id' => 7, 'medication_id' => 8 ]);
+        DiseaseMedication::create([ 'disease_id' => 7, 'medication_id' => 10 ]);
+        DiseaseMedication::create([ 'disease_id' => 7, 'medication_id' => 11 ]);
+        DiseaseMedication::create([ 'disease_id' => 7, 'medication_id' => 6 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 8, 'medication_id' => 20 ]);
+        DiseaseMedication::create([ 'disease_id' => 8, 'medication_id' => 7 ]);
+        DiseaseMedication::create([ 'disease_id' => 8, 'medication_id' => 8 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 9, 'medication_id' => 6 ]);
+        DiseaseMedication::create([ 'disease_id' => 9, 'medication_id' => 5 ]);
+        DiseaseMedication::create([ 'disease_id' => 9, 'medication_id' => 21 ]);
+        DiseaseMedication::create([ 'disease_id' => 9, 'medication_id' => 22 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 10, 'medication_id' => 1 ]);
+        DiseaseMedication::create([ 'disease_id' => 10, 'medication_id' => 4 ]);
+        DiseaseMedication::create([ 'disease_id' => 10, 'medication_id' => 24 ]);
+        DiseaseMedication::create([ 'disease_id' => 10, 'medication_id' => 25 ]);
+        DiseaseMedication::create([ 'disease_id' => 10, 'medication_id' => 7 ]);
+        DiseaseMedication::create([ 'disease_id' => 10, 'medication_id' => 9 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 11, 'medication_id' => 14 ]);
+        DiseaseMedication::create([ 'disease_id' => 11, 'medication_id' => 5 ]);
+        DiseaseMedication::create([ 'disease_id' => 11, 'medication_id' => 13 ]);
+
+        DiseaseMedication::create([ 'disease_id' => 12, 'medication_id' => 1 ]);
+        DiseaseMedication::create([ 'disease_id' => 12, 'medication_id' => 22 ]);
+        DiseaseMedication::create([ 'disease_id' => 12, 'medication_id' => 26 ]);
+        DiseaseMedication::create([ 'disease_id' => 12, 'medication_id' => 11 ]);
+
     }
 }
